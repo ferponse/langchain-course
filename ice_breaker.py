760847d1cd01bdbcdbb62a68f817e32f35a9c333
Ferran Pons Serra <ferranponsuniversidad@gmail.com>
2024-10-13 00:38:16 +0200

    Connect ice breaker app with linkedin scrapper

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,21 +1,12 @@
 import os
+from typing import Any, Dict
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.schema import AIMessage
 from langchain.chains import LLMChain
 
-information: str = """
-Elon Reeve Musk (Pretoria, 28 de junio de 1971), conocido como Elon Musk, es un empresario, inversor, activista político conservador5​6​ y magnate sudafricano que también posee las nacionalidades canadiense y estadounidense. Es el fundador, consejero delegado e ingeniero en jefe de SpaceX; inversor ángel, director general y arquitecto de productos de Tesla, Inc.; fundador de The Boring Company; cofundador de Neuralink y OpenAI, aunque ya no tiene más participación en esta última por desacuerdos en el rumbo de la empresa. Además de ser el director de tecnología de X Corp..7​
-
-Con un patrimonio neto estimado en unos 252.4 mil millones de dólares en julio de 2024,8​ llegando a ser la persona más rica del mundo según el índice de multimillonarios en tiempo real de Forbes.9​10​
-
-Musk nació y se crío en una rica familia de Pretoria (Sudáfrica). Su madre es canadiense y su padre un sudafricano blanco. Estudió brevemente en la Universidad de Pretoria antes de trasladarse a Canadá a los 17 años. Se matriculó en la Universidad de Queen y se trasladó a la Universidad de Pensilvania dos años después, donde se graduó en Economía y Física. En 1995 se trasladó a California para asistir a la Universidad Stanford, pero en su lugar decidió seguir una carrera empresarial, cofundando la empresa de software web Zip2 con su hermano Kimbal. La empresa fue adquirida por Compaq por 307 millones de dólares en 1999. Ese mismo año, Musk cofundó el banco en línea X.com, que se fusionó con Confinity en 2000 para formar PayPal. La empresa fue comprada por eBay en 2002 por 1500 millones de dólares.
-
-En 2002, Musk fundó SpaceX, fabricante aeroespacial y empresa de servicios de transporte espacial, de la que es CEO e ingeniero jefe. En 2003, se unió al fabricante de vehículos eléctricos Tesla Motors, Inc. (ahora Tesla, Inc.) como presidente y arquitecto de productos, convirtiéndose en su consejero delegado en 2008. En 2006, ayudó a crear SolarCity, una empresa de servicios de energía solar que posteriormente fue adquirida por Tesla y se convirtió en Tesla Energy. En 2015, cofundó OpenAI, una empresa de investigación sin ánimo de lucro que promueve la inteligencia artificial amigable. En 2016, cofundó Neuralink, una empresa de neurotecnología centrada en el desarrollo de interfaces cerebro-ordenador, y fundó The Boring Company, una empresa de construcción de túneles. También acordó la compra de la importante red social estadounidense Twitter en 2022 por 44 000 millones de dólares. Musk también ha propuesto el hyperloop. En noviembre de 2021, el director general de Tesla fue la primera persona de la historia en acumular una fortuna de 300 000 millones de dólares.11​
-
-Ha sido criticado por hacer declaraciones poco científicas y controvertidas. En 2018, fue demandado por la Comisión de Bolsa y Valores de Estados Unidos (SEC) por tuitear falsamente que había conseguido financiación para una adquisición privada de Tesla. Llegó a un acuerdo con la SEC pero no admitió su culpabilidad, renunciando temporalmente a su presidencia y aceptando limitaciones en su uso de Twitter. En 2019, ganó un juicio por difamación presentado contra él por un espeleólogo británico que asesoró en el rescate de la cueva de Tham Luang. Musk también ha sido criticado por difundir información errónea sobre la pandemia de COVID-19 y teorías de conspiración; y por sus otras opiniones sobre asuntos como la inteligencia artificial, las criptomonedas y el transporte público.
-"""
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
     print("Hello LangChain!")
@@ -23,7 +14,7 @@ if __name__ == "__main__":
     load_dotenv()
 
     summary_template: str = """
-        Dada la información {information} sobre una persona, quiero que crees:
+        Dada la información de LinkedIn {information} sobre una persona, quiero que crees:
         1. Un resumen breve
         2. Dos datos interesantes sobre ella
     """
@@ -34,6 +25,8 @@ if __name__ == "__main__":
 
     chain: LLMChain = summary_prompt_template | llm
 
-    result: AIMessage = chain.invoke(input={"information": information})
+    linkedin_data: Dict[str, Any] = scrape_linkedin_profile(linkedin_profile_url="https://linkedin.com/in/ferranponsdev/", mock=True)
+
+    result: AIMessage = chain.invoke(input={"information": linkedin_data})
 
     print(result.content)
