5b65acd77af98eab03ecc462e2f976375d6dd4fc
Ferran Pons Serra <ferranponsuniversidad@gmail.com>
2024-10-13 12:39:04 +0200

    Add Pydantic output parser to parse the response of the llm

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,32 +1,36 @@
-import os
-from typing import Any, Dict
 from dotenv import load_dotenv
+from langchain.chains import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.schema import AIMessage
-from langchain.chains import LLMChain
 
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 
-if __name__ == "__main__":
-    print("Hello LangChain!")
 
-    load_dotenv()
+def ice_break_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url, mock=True)
 
     summary_template: str = """
         Dada la información de LinkedIn {information} sobre una persona, quiero que crees:
         1. Un resumen breve
         2. Dos datos interesantes sobre ella
+
+        {format_instructions}
     """
 
-    summary_prompt_template: PromptTemplate = PromptTemplate(template=summary_template, input_variables=["information"])
+    summary_prompt_template: PromptTemplate = PromptTemplate(template=summary_template, input_variables=["information"], partial_variables={"format_instructions": summary_parser.get_format_instructions()})
 
     llm: ChatOpenAI = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    
+    chain: LLMChain = summary_prompt_template | llm | summary_parser
 
-    chain: LLMChain = summary_prompt_template | llm
-
-    linkedin_data: Dict[str, Any] = scrape_linkedin_profile(linkedin_profile_url="https://linkedin.com/in/ferranponsdev/", mock=True)
-
-    result: AIMessage = chain.invoke(input={"information": linkedin_data})
+    result: Summary = chain.invoke(input={"information": linkedin_data})
 
     print(result.content)
+
+if __name__ == "__main__":
+    load_dotenv()
+    ice_break_with(name="Ferran Pons Serra Linkedin IA")
+    
